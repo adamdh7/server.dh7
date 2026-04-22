3c2ba8ef96da7f6a6b10c566c7c4e92fc962593c
Adam_D’H7 <tergenefocus@gmail.com>
2026-04-21 20:26:05 -0400

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const ALLOWED_ORIGINS = new Set(['https://dh7.dh7.adamdh7.org', 'https://dh7.adamdh7.org', 
-'https://ai.adamdh7.org',
+'https://ai.adamdh7.org', 'https://mizik.adamdh7.org',
 'https://server.ai.adamdh7.org',               'https://quiz.adamdh7.org', 'https://dh7test.pages.dev', 'https://www.adamdh7.org']);
 const ALLOWED_HOSTS = new Set(['dh7.adamdh7.org', 'quiz.pages.dev', 'ai.adamdh7.org', 'www.adamdh7.org']);
 const WORKER_TOKEN = process.env.WORKER_TOKEN || '';
